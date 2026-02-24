279b450cafd821308c6e598fbc40f221efce5a88
C. Witt <c.wittekind@posteo.de>
2026-02-24 12:32:02 +0100

    removed extra check

--- app/convert_optain.R
+++ app/convert_optain.R
@@ -106,7 +106,6 @@ if(mode == "fast"){
   })
   
   
-  
 land_u = c("hedge", "buffer","edgefilter","grasshedge", "shrubhedge", "grassbuffer", "shrubbuffer", "grassslope","grassland","grassrchrg", "terrace", "floodres","swale", "rip_forest", "afforest", "afforestation", "contr")
 
 ## check, assign and write priorities, hardcodes what has been used in CoMOLA
@@ -141,7 +140,6 @@ nswrm_priorities <- function(lu) {
   return(prio)
 }
 
-
 ## Genomes
   gen = read.table("../data/pareto_genomes.txt", header = FALSE, stringsAsFactors = FALSE, sep = deli("../data/pareto_genomes.txt"), encoding = "UTF-8")
   fit = read.table("../data/pareto_fitness.txt", header = FALSE, stringsAsFactors = FALSE, sep = deli("../data/pareto_fitness.txt"))
@@ -157,10 +155,6 @@ nswrm_priorities <- function(lu) {
 
 # genome_hru matches AEP with hrus, several hrus for each AEP (hru = obj_id)
   genome_hru <- read.csv("../data/measure_location.csv")
-  if (nrow(gen) != nrow(genome_hru)) {#CS6-inspired check for inconsistent MOO outputs
-    stop("Error: the measure_location.csv and pareto_genomes.txt files have different numbers of rows. Please check your input files - are they from the same model run?")
-  }
-  
   
   print("check: read measure_location.csv...", quote = FALSE)
   
